e5d8076abcfbe3239f9803cb9a119f49bb4fc778
SB <55782417+SuPeRBoY321@users.noreply.github.com>
2025-10-03 19:14:31 +0300

    Update backupdir.sh

--- backup/backupdir.sh
+++ backup/backupdir.sh
@@ -1,2 +1,24 @@
 #!/bin/bash
 
+CONF_FILE="backup_conf.json"
+#такая конструкция подходит чтобы проверять есть ли пакет в системе
+if ! command -v jq &> /dev/bull; then
+  echo "Error"
+  exit 1
+fi
+
+BACKUP_COUNT=$(jq '.backups | length' "$CONFIG_FILE_JSON")
+
+for (( i=0; i<BACKUP_COUNT; i++));  do
+  BACKUP_NAME=$(jq -r ".backup[$i].backup_name" "$CONFIG_FILE_JSON")
+  SOURCE_FOLDER=$(jq -r ".backup[$i].source_folder" "$CONFIG_FILE_JSON")
+  TARGET_FOLDER=$(jq -r ".backup[$i].target_folder" "$CONFIG_FILE_JSON")
+
+  echo "---Start backup: $BACKUP_NAME"
+  
+  if [ ! -d "$SOURCE_FOLDER" ]; then
+    echo "---Skipping $BACKUP_NAME: Source folder $SOURCE_FOLDER dose not exist"
+    continue #i=i+1
+  fi
+
+done
